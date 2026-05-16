f0e97684201b59b8bc20354430b48bc12e04299a
jasonburby-creator <jason.burby@gmail.com>
2026-05-16 11:38:08 -0700

    Add files via upload
    initial commit round 3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Don't fail Vercel builds on lint warnings.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
